5cc4e6b404694b2f6b183d4380eb7d7f30b13378
ana <ana.souza0087@gmail.com>
2026-01-06 19:15:00 -0300

    feat: finish routes and update database

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,21 @@
+
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
+
   def index
     @posts = Post.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @posts }
+    end
   end
 
   def show
-    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.json { render json: @post }
+    end
   end
 
   def new
@@ -13,15 +24,48 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-    if @post.save
-      redirect_to @post
-    else
-      render :new, status: :unprocessable_entity
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "Post created successfully" }
+        format.json { render json: @post, status: :created }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: { errors: @post.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @post.update(post_params)
+        format.html { redirect_to @post, notice: "Post updated successfully" }
+        format.json { render json: @post }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: { errors: @post.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    respond_to do |format|
+      format.html { redirect_to posts_path, notice: "Post deleted successfully" }
+      format.json { head :no_content }
     end
   end
 
   private
-    def post_params
-      params.expect(post: [ :title, :text ])
-    end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_12_22_213509) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_06_221028) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
-    t.datetime "date"
     t.string "text"
     t.string "theme"
     t.string "title"
